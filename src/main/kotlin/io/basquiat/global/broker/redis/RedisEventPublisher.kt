9e6990f41a93a2b316d3c027ac7385256d8b19a1
Basquiat Yoon <funnyjazz@naver.com>
2026-02-11 14:51:38 +0900

    update: pub/sub stream 방식 코드 업데이트
    Signed-off-by: beatoz-basquiat <bsyoon@beatoz.io>

--- src/main/kotlin/io/basquiat/global/broker/redis/RedisEventPublisher.kt
+++ src/main/kotlin/io/basquiat/global/broker/redis/RedisEventPublisher.kt
@@ -6,6 +6,7 @@ import io.basquiat.global.broker.common.code.BrokerChannel
 import io.basquiat.global.utils.logger
 import io.basquiat.global.utils.notMatchMessageType
 import org.redisson.api.RedissonClient
+import org.redisson.api.stream.StreamAddArgs
 import org.springframework.stereotype.Service
 
 @ConditionalOnRedis
@@ -22,11 +23,31 @@ class RedisEventPublisher(
         // RedisChannel에 정의된 타입과 메시지 타입이 일치하는지 확인한다.
         if (!channel.type.isInstance(message)) notMatchMessageType("채널 ${channel.channelName}에 맞지 않는 메시지 타입입니다.")
 
-        val topic = redissonClient.getTopic(channel.channelName)
-        topic
-            .publishAsync(message)
-            .thenAccept { subscriber ->
-                log.info("subscriber: $subscriber")
+        val stream = redissonClient.getStream<String, T>(channel.channelName)
+
+        stream
+            .addAsync(StreamAddArgs.entry("payload", message))
+            .thenAccept { id ->
+                log.info("Successfully Redis subscriber id: $id")
+            }.exceptionally { e ->
+                log.error("Failed to publish message to Redis Stream: ${e.message}")
+                null
             }
     }
+
+// 처음 시도한 방식
+//    override fun <T : Any> publish(
+//        channel: BrokerChannel,
+//        message: T,
+//    ) {
+//        // RedisChannel에 정의된 타입과 메시지 타입이 일치하는지 확인한다.
+//        if (!channel.type.isInstance(message)) notMatchMessageType("채널 ${channel.channelName}에 맞지 않는 메시지 타입입니다.")
+//
+//        val topic = redissonClient.getTopic(channel.channelName)
+//        topic
+//            .publishAsync(message)
+//            .thenAccept { subscriber ->
+//                log.info("subscriber: $subscriber")
+//            }
+//    }
 }
